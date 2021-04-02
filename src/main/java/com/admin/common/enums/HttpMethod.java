6d6055e838969f5518a601ee39b2ca41f9ddb9c3
hyj <17364524550@163.com>
2021-04-02 23:08:53 +0800

    添加注释

--- src/main/java/com/admin/common/enums/HttpMethod.java
+++ src/main/java/com/admin/common/enums/HttpMethod.java
@@ -15,6 +15,7 @@ public enum HttpMethod
 
     private static final Map<String, HttpMethod> mappings = new HashMap<>(16);
 
+   //随着类的加载而执行，而且只执行一次,将http请求方法加入到hashmap
     static
     {
         for (HttpMethod httpMethod : values())
@@ -23,12 +24,22 @@ public enum HttpMethod
         }
     }
 
+    /**
+     * 返回该方法的枚举变量
+     * @param method
+     * @return
+     */
     @Nullable
     public static HttpMethod resolve(@Nullable String method)
     {
         return (method != null ? mappings.get(method) : null);
     }
 
+    /**
+     * 是否匹配
+     * @param method
+     * @return
+     */
     public boolean matches(String method)
     {
         return (this == resolve(method));
